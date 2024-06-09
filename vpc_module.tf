a2d62af3e100f5a7754cd344b8e1655a6cb684e7
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-09 08:08:11 -0400

    Update vpc_module.tf

--- vpc_module.tf
+++ vpc_module.tf
@@ -1,17 +1,17 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.4.0"
-  name = "${var.tags}-${var.vpc_name}"
+  name = "${local.name}-${var.vpc_name}"
   cidr = var.vpc_cidr_block
   azs             = var.vpc_availability_zones
   public_subnets  = var.vpc_public_subnets
   private_subnets = var.vpc_private_subnets  
-  tags = var.tags
-  vpc_tags = var.tags
+  tags = local.common_tags
+  vpc_tags = local.common_tags
+
   public_subnet_tags = {
     Type = "Public Subnets"
   }
   private_subnet_tags = {
     Type = "Private Subnets"
   }  
-}
