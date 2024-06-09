ce416dd22b2d68e4acb27cd2fa428138e8a9bf48
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-09 08:47:00 -0400

    Create wafdata.tf

---
+++ wafdata.tf
@@ -0,0 +1,19 @@
+resource "aws_wafv2_web_acl" "example" {
+  name  = "web-acl-association-example"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
+  }
+}
+
+resource "aws_wafv2_web_acl_association" "example" {
+  resource_arn = aws_api_gateway_stage.example.arn
+  web_acl_arn  = aws_wafv2_web_acl.example.arn
+}
