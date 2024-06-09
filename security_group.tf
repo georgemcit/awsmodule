7fca2d1ca315427cc15c4272759c6d77514c05e6
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 23:23:42 -0400

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -1,1 +1,11 @@
-
+module "george_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"  
+  
+  name = "george-sg"
+  vpc_id = module.vpc.vpc_id
+  ingress_rules = ["ssh-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  tags = local.common_tags
+}
