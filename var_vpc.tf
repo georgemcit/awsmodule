e320f2c19f37606d2e282c5cb0a7fc04a4eff04f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-08 23:36:37 -0400

    Update var_vpc.tf

--- var_vpc.tf
+++ var_vpc.tf
@@ -15,11 +15,13 @@ variable "vpc_public_subnets" {
   default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 variable "vpc_private_subnets" {
-  description = "VPC Private Subnets"
   type = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
-
+variable "tags" {
+  type = string
+  default = georgeibrahim
+}
 
 
 
